f776aeb0caedac43db99e6691617935e9ba23816
rodrigotrevisan12-netizen <rodrigotrevisan12@gmail.com>
2026-07-17 09:05:17 -0300

    Replace Input with PasswordInput component

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -7,6 +7,7 @@ import { toast } from "sonner";
 import { supabase } from "@/integrations/supabase/client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { PasswordInput } from "@/components/ui/password-input";
 import { Label } from "@/components/ui/label";
 import { checkLoginLock, recordFailedLogin, recordSuccessfulLogin } from "@/lib/login-security.functions";
 import { verifyTurnstileToken } from "@/lib/turnstile.functions";
@@ -235,9 +236,8 @@ function AuthPage() {
                       Esqueci a senha
                     </button>
                   </div>
-                  <Input
+                  <PasswordInput
                     id="password"
-                    type="password"
                     autoComplete="current-password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
